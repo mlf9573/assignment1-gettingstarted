7c33d3f0ff22a26613d25063b10a5a6665a6b7cf
mlf9573 <michael.frenk@nyu.edu>
2024-01-23 19:56:29 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,9 +6,23 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = ""
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = ""
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = ""
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = ""
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = ""
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = ""
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = ""
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
